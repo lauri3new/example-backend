81c7688987ce5cdf83a501673c92ed47de53dd8b
lauri3new <laurence@wearepercent.com>
2022-03-27 22:50:24 +0100

    del

--- src/animals/helpers/index.ts
+++
@@ -1,9 +0,0 @@
-import { Left, Right } from 'light-fp/dist/Either'
-import { AnimalType } from '../respositories/animal'
-
-export const checkAnimalType = (a: string) => {
-  if (['dog', 'cat', 'elephant', 'sloth'].includes(a)) {
-    return Right(a as AnimalType)
-  }
-  return Left('not a valid animal' as const)
-}
--- src/lib/index.ts
+++
@@ -1,8 +0,0 @@
-import { Left, Right, Either } from 'light-fp/dist/Either'
-
-export const fromNullable = <A>(a: null | undefined | A): Either<null, A> => {
-  if (a == null) {
-    return Left(null)
-  }
-  return Right(a)
-}
